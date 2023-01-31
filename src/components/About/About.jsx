6054e263ae3414492a65d13ece6e9a50f43b49f1
JayashTripathy <jayashtripathy371@gmail.com>
2023-02-01 00:39:15 +0530

    project files all set( css variables declared, file structuring  done

---
+++ src/components/About/About.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../About/About.css"
+
+export const About = () => {
+  return (
+    <section className='about-section'>About</section>
+  )
+}
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../Hero/Hero.css"
+
+export const Hero = () => {
+  return (
+    <section className='hero-section'>Hero</section>
+  )
+}
