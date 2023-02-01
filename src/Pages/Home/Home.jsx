ff7e171b5f03e2fd7bea82ee66c840df690712a9
JayashTripathy <76092296+JayashTripathy@users.noreply.github.com>
2023-02-01 23:57:24 +0530

    Revert "Yash navbar"

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { About } from '../../components/About/About'
 import { Hero } from '../../components/Hero/Hero'
-
 import "../Home/Home.css"
 
 export const Home = () => {
   return (
     <div className='homepage-container'>
-        
         <Hero/>
         <About/>
     </div>
