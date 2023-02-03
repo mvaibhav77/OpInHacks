6bb936f74f39c3337b46da9f04ccf28c819fb3dc
Rishika Jain <77011730+rjrishika@users.noreply.github.com>
2023-02-03 21:44:12 +0530

    res changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,17 +9,10 @@ import Pillarlarge from '../../assets/pillar-large.png'
 export const About = () => {
   return (
     <section className='about-section'>
-     
-        <h1 className="about">About Us</h1>
-        <p className="para">Welcome to Opinhacks, the ultimate student hackathon where imagination and technology collide! We're a community of tech-savvy students who come together to push the limits of what's possible. Our mission is to provide a platform for students to bring their boldest and most innovative ideas to life.
-        <br></br>
-        <br></br>
-
-Get ready for a weekend of intense collaboration, coding, and creativity. Whether you're a seasoned programmer, a design wizard, or just someone with a passion for technology, Opinhacks is the perfect place to showcase your skills. Meet new friends, build a network of like-minded individuals, and be part of a community that's shaping the future of technology.
-</p>
+       
       
        
-
+       <img className="col3" src={Pillarmed} />
       <img className="col2" src={Pillarsmall} />
 
         <img className="col1" src={Pillarmed} />
@@ -29,11 +22,19 @@ Get ready for a weekend of intense collaboration, coding, and creativity. Whethe
         <img className="col5" src={Pillarlarge} />*/}
          <img className="col8" src={Pillar} />
   <img className="col6" src={Pillar} />
-  <img className="col3" src={Pillarmed} />
+ 
        <img className="col4" src={Pillarmed} />
         <img className="col" src={Pillar} /> 
         
-     
+        <div style={{width:"100%"}}>
+        <h1 className="about">About Us</h1>
+        <p className="para">Welcome to Opinhacks, the ultimate student hackathon where imagination and technology collide! We're a community of tech-savvy students who come together to push the limits of what's possible. Our mission is to provide a platform for students to bring their boldest and most innovative ideas to life.
+        <br></br>
+        <br></br>
+
+Get ready for a weekend of intense collaboration, coding, and creativity. Whether you're a seasoned programmer, a design wizard, or just someone with a passion for technology, Opinhacks is the perfect place to showcase your skills. Meet new friends, build a network of like-minded individuals, and be part of a community that's shaping the future of technology.
+</p>
+</div>
     </section>
   )
 }
