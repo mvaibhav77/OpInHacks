a51d5a03d2e66febffeecb245a6dbcec44ae3a71
Rishika Jain <77011730+rjrishika@users.noreply.github.com>
2023-02-05 11:51:42 +0530

    text and res

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,26 +9,16 @@ import Pillarlarge from '../../assets/pillar-large.png'
 export const About = () => {
   return (
     <section className='about-section'>
-       
-      
-       
        <img className="col3" src={Pillarmed} />
-      <img className="col2" src={Pillarsmall} />
-
-        <img className="col1" src={Pillarmed} />
-        
-         {/*<img className="col7" src={Pillarmed} />
-       
-        <img className="col5" src={Pillarlarge} />*/}
-         <img className="col8" src={Pillar} />
-  <img className="col6" src={Pillar} />
- 
+       <img className="col2" src={Pillarsmall} />
+       <img className="col1" src={Pillarmed} />
+       <img className="col8" src={Pillar} />
+       <img className="col6" src={Pillar} />
        <img className="col4" src={Pillarmed} />
-        <img className="col" src={Pillar} /> 
-        
-        <div style={{width:"100%"}}>
-        <h1 className="about">About Us</h1>
-        <p className="para">Whether you're a seasoned developer or just starting out, OpInHacks is the perfect platform to challenge yourself, develop new skills, and network with like-minded individuals. 
+       <img className="col" src={Pillar} /> 
+       <div style={{width:"100%"}}>
+       <h1 className="about">About Us</h1>
+       <p className="para">Whether you're a seasoned developer or just starting out, OpInHacks is the perfect platform to challenge yourself, develop new skills, and network with like-minded individuals. 
 OpInHacks will be a thrilling and dynamic OFFLINE hackathon event designed to bring together coders from all backgrounds and levels of expertise. With a focus on fostering a competitive and inclusive environment, this 36-hour hackathon will feature multiple tracks, including both tech and non-tech, to cater to a diverse range of interests.
 With an emphasis on learning and growth, OpInHacks is not just about winning, but about the journey of becoming a better developer. 
 </p>
