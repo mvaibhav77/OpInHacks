31fc0f7125fe99f0bac1d2c296d7b72e8ef86f87
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2023-08-29 12:10:56 +0530

    Tailwind Complete except Hero.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,9 +6,9 @@ import { FaDiscord } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 import { AiFillLinkedin } from "react-icons/ai";
 import { AiOutlineTwitter } from "react-icons/ai";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
-import BacktoFuture from "../../assets/BacktoFuture.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import BacktoFuture from "../../assets/BacktoFuture.png";
 
 const Footernew = () => {
   function discordlink() {
@@ -21,65 +21,122 @@ const Footernew = () => {
     window.open("https://forms.gle/xzP1vVgXWMBpSdHF9");
   }
   return (
-    <footer id="Footer" class="padding_4x">
-      <div class="flex">
-        <section class="flex-content padding_1x">
-          <img src={logo} alt="Logo" className="logo" />
-          <p className="content-p">A battle through time.</p>
-          <div className="btn-div">
-            <button className="btn register" onClick={registration}>
+    <footer id="Footer" className="p-8 md:p-4 bg-dark text-light">
+      <div className="grid grid-cols-4 md:grid-cols-2">
+        <section className="flex-content w-full relative flex flex-col p-4">
+          <img src={logo} alt="Logo" className="logo w-1/2 mb-2 md:w-full" />
+          <p className="content-p font-tertiary opacity-60 mb-2">
+            A battle through time.
+          </p>
+          <div className="btn-div flex flex-wrap md:flex-col max-w-[90%] md:max-w-full">
+            <button
+              className="btn register bg-primary text-dark text-base w-3/5 mr-[4%] md:mr-0 md:w-full md:my-0.5 md:mx-0 p-3"
+              onClick={registration}
+            >
               REGISTER
             </button>
-            <button className="btn discord" onClick={discordlink}>
-              <FaDiscord color="black" size="2rem" />
+            <button
+              className="btn discord text-base max-w-100 py-2 px-4 md:my-0.5 md:mx-0"
+              onClick={discordlink}
+            >
+              <FaDiscord color="black" size="2rem" className="mx-auto" />
             </button>
-            <button className="btn sponsor" onClick={sponsor_us}>
+            <button
+              className="btn sponsor text-base p-4 w-4/5 mt-[4%] text-dark bg-white md:w-full md:my-0.5 md:mx-0"
+              onClick={sponsor_us}
+            >
               SPONSOR US
             </button>
           </div>
         </section>
-        <section class="flex-content padding_1x">
-          <h3>Site</h3>
-          <a href="#Hero">Home</a>
-          <a href="#About">About</a>
-          <a href="#Organisers">Organisers</a>
-          <a href="#sponsors-list">Sponsors</a>
-          <a href="#Faq">FAQ</a>
+        <section className="flex-content relative flex flex-col p-4 pl-8 font-tertiary">
+          <h3 className="text-lg font-primary text-light mb-4">Site</h3>
+          <a
+            className="font-tertiary text-light my-2 mx-0 opacity-70"
+            href="#Hero"
+          >
+            Home
+          </a>
+          <a
+            className="font-tertiary text-light my-2 mx-0 opacity-70"
+            href="#About"
+          >
+            About
+          </a>
+          <a
+            className="font-tertiary text-light my-2 mx-0 opacity-70"
+            href="#Organisers"
+          >
+            Organisers
+          </a>
+          <a
+            className="font-tertiary text-light my-2 mx-0 opacity-70"
+            href="#sponsors-list"
+          >
+            Sponsors
+          </a>
+          <a
+            className="font-tertiary text-light my-2 mx-0 opacity-70"
+            href="#Faq"
+          >
+            FAQ
+          </a>
         </section>
-        <section class="flex-content padding_1x">
-          <h3 className="social-text">Socials</h3>
-          <div className="flex-socials">
-          <a
-            href="https://www.instagram.com/opinhacks/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <AiFillInstagram  size="2rem" className="socials-icon"/>
-          </a>
-          <a
-            href="https://www.linkedin.com/company/opinhacks/"
-            target="_blank"
-            rel="noreferrer"
-          >
-              <AiFillLinkedin size="2rem" className="socials-icon"/>
-          </a>
-          <a
-            href="https://twitter.com/opinhacks"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FontAwesomeIcon icon={faXTwitter} size="2x" className="socials-icon" />
-          </a>
+        <section className="flex-content relative flex flex-col p-4">
+          <h3 className="social-text ml-4 font-primary text-lg text-light mb-4">
+            Socials
+          </h3>
+          <div className="flex-socials w-full flex flex-row flex-nowrap gap-4">
+            <a
+              className="font-tertiary text-light my-2 mx-0 opacity-70"
+              href="https://www.instagram.com/opinhacks/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillInstagram
+                size="2rem"
+                className="socials-icon text-primary"
+              />
+            </a>
+            <a
+              className="font-tertiary text-light my-2 mx-0 opacity-70"
+              href="https://www.linkedin.com/company/opinhacks/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillLinkedin
+                size="2rem"
+                className="socials-icon text-primary"
+              />
+            </a>
+            <a
+              className="font-tertiary text-light my-2 mx-0 opacity-70"
+              href="https://twitter.com/opinhacks"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faXTwitter}
+                size="2x"
+                className="socials-icon text-primary"
+              />
+            </a>
           </div>
         </section>
         <section>
-          <img className="back-to-future-img" src={BacktoFuture} alt="" />
+          <img
+            className="back-to-future-img flex md:w-80"
+            src={BacktoFuture}
+            alt=""
+          />
         </section>
       </div>
-      <hr className="hr-tag" />
-      <div class="flex">
-        <section class="flex-content padding_1x">
-          <p className="disclaimer">Made with <span className="heart">&#10084;</span></p>
+      <hr className="hr-tag w-4/5 opacity-30 mx-auto" />
+      <div className="flex">
+        <section className="flex-content relative flex p-4 pb-0 w-full">
+          <p className="disclaimer mx-auto flex justify-center items-center gap-1.5 font-tertiary">
+            Made with <span className="heart text-lg text-[red]">&#10084;</span>
+          </p>
         </section>
       </div>
     </footer>
